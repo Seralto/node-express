cf5616d08e314534f72986586f050c4200e52dfb
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-12-03 17:04:09 -0200

    Update User routes

--- routes/users.js
+++ routes/users.js
@@ -16,9 +16,28 @@ router.get('/', async (_, res) => {
   res.send(user);
 });
 
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+});
+
 router.post('/', async (req, res) => {
   let user = new User({ name: req.body.name })
   user = await user.save();
+  res.send(user);
+});
+
+router.put('/:id', async (req, res) => {
+  const user = await User.updateOne({
+    name: req.body.name
+  });
+
+  res.send(user);
+});
+
+router.delete('/:id', async (req, res) => {
+  const user = await User.findOneAndDelete(req.params.id);
   res.send(user);
 });
 
